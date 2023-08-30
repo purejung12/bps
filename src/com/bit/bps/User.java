ab18024489be8c3b76d6b11d3fbeaf40a33bbac1
purejung12 <purejung12@gmail.com>
2023-08-30 14:43:18 +0900

    2st commit

--- src/com/bit/bps/User.java
+++ src/com/bit/bps/User.java
@@ -3,7 +3,7 @@ package com.bit.bps;
 public class User {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        System.out.println("hello~ github");
 
     }
 
